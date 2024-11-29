124cf86ff0ff7a2a78c488b0feaed708618a0e55
Ducdelauzun <gadnsaka@gmail.com>
2024-11-29 11:22:50 +0100

    Préparation du formulaire pour modification des jeux, enregistrement sur GitHub

--- script.js
+++ script.js
@@ -84,3 +84,42 @@ function writeDom() {
 }
 
 writeDom()
+
+const editButtons = document.querySelectorAll(".edit")
+editButtons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		editModal(e.target.getAttribute("data-edit-id"))
+	})
+})
+
+const viewButtons = document.querySelectorAll(".view")
+viewButtons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		viewModal(e.target.getAttribute("data-view-id"))
+	})
+})
+
+function modifyModal(modalTitle, modalBody) {
+	// Écrir le nom du jeu dans le titre du modal
+	document.querySelector(".modal-title").textContent = modalTitle
+    console.log(modalTitle)
+	document.querySelector(".modal-body").innerHTML = modalBody
+}
+
+
+function viewModal(gameId) {
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
+	modifyModal(gamesList[result].title, modalBody)
+}
+
+function editModal(gameId) {
+	console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	// passer une image comme corps du modal
+	const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
+	modifyModal("Mode Edition", modalBody)
+}
